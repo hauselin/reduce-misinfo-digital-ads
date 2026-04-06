71a40160cdaca94aa9cd3ed9863f3e5db27782f8
Hause Lin <hauselin@gmail.com>
2026-04-06 13:48:49 -0500

    Clean up code

---
+++ src/twitter-exps-treatment-threshold-70.R
@@ -0,0 +1,11 @@
+# %% set up =================================================
+
+rm(list = ls())
+library(tidyverse)
+library(data.table)
+library(glue)
+library(patchwork)
+library(here)
+theme_set(theme_minimal())
+
+# %%
--- src/twitter-meta-treatment-thresholds.R
+++ src/twitter-meta-treatment-thresholds.R
@@ -1,4 +1,4 @@
-# %%
+# %% set up =================================================
 
 rm(list = ls())
 library(tidyverse)
@@ -13,6 +13,19 @@ length(files)
 
 # %%
 
+get_result <- function(model, threshold) {
+    return(
+        data.table(
+            threshold = threshold,
+            estimate = model$TE.fixed,
+            se = model$seTE.fixed,
+            low = model$lower.fixed,
+            high = model$upper.fixed,
+            pval = model$pval.fixed
+        )
+    )
+}
+
 models_itt <- list()
 models_att <- list()
 meta_itt_results <- list()
@@ -26,35 +39,19 @@ for (i in 1:length(files)) {
     thresholds <- c(thresholds, threshold)
 
     # fit ITT model
-    m0 <- metagen(TE = raw_estimate, seTE = raw_estimate_se, data = d2, method.tau = "REML", fixed = TRUE)
+    m0 <- metagen(TE = raw_estimate, seTE = raw_estimate_se, data = d2, method.tau = "REML", common = TRUE)
     print(m0)
     models_itt[[i]] <- m0
 
     # fit ATT model
-    m1 <- metagen(TE = raw_estimate_att, seTE = se_att, data = d2, method.tau = "REML", fixed = TRUE)
+    m1 <- metagen(TE = raw_estimate_att, seTE = se_att, data = d2, method.tau = "REML", common = TRUE)
     print(m1)
     models_att[[i]] <- m1
 
-    # save ITT results
-    d0 <- data.table(
-        threshold = threshold,
-        estimate = m0$TE.fixed,
-        se = m0$seTE.fixed,
-        low = m0$lower.fixed,
-        high = m0$upper.fixed,
-        pval = m0$pval.fixed
-    )
+    d0 <- get_result(m0, threshold)
     meta_itt_results[[i]] <- d0
 
-    # save ATT results
-    d1 <- data.table(
-        threshold = threshold,
-        estimate = m1$TE.fixed,
-        se = m1$seTE.fixed,
-        low = m1$lower.fixed,
-        high = m1$upper.fixed,
-        pval = m1$pval.fixed
-    )
+    d1 <- get_result(m1, threshold)
     meta_att_results[[i]] <- d1
 }
 names(models_itt) <- thresholds
@@ -62,16 +59,14 @@ names(models_att) <- thresholds
 names(meta_itt_results) <- thresholds
 names(meta_att_results) <- thresholds
 
-# %% main result: threshold 70
+# %% main result: quality threshold 70
 
-# main result: threshold 70
-thres70_itt <- meta_itt_results[["70"]]
-thres70_itt
-summ_metagen(models_itt[["70"]])
+thres <- "70"
+meta_itt_results[[thres]]
+summ_metagen(models_itt[[thres]])
 
-thres70_att <- meta_att_results[["70"]]
-thres70_att
-summ_metagen(models_att[["70"]])
+meta_att_results[[thres]]
+summ_metagen(models_att[[thres]])
 
 # %% results for all thresholds from 40 to 80, in steps of 5 (S2.2.3 multiverse analysis)
 
